e8d6e1b6759a8239d17a5fbbdef832b5d09d045b
JuanBSoares <juanbsoares@outlook.com>
2024-10-11 23:14:16 -0300

    adding task day 1

---
+++ day-1/task_day1.py
@@ -0,0 +1,31 @@
+#Day 1
+
+#function: print()
+print("Hello World!")
+
+#Printing Many Lines
+
+##TODO
+
+##String Manipulation, Concatenation and Code Intelligence
+print("Hello World!\nHello World!\nHello World!\n")
+
+print ("Hello" + " " + "Juan")
+
+#Inputs
+#function: input()
+print("Hello," + " " + input("What's your name? ") + "!")
+
+#Variables
+print("Variables Section\n")
+name = input("What's your name? ")
+print(name)
+#Printing the length of the variable
+#function: len()
+print(len(name))
+
+print(len(input("What's your name? ")))
+
+username = input("What's your username? ")
+length = len(username)
+print(length)
