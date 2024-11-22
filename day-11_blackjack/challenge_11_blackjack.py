8b2c51e1abce79fa99b7d34e9fdd45e76c401dd0
JuanBSoares <juanbsoares@outlook.com>
2024-11-21 22:45:55 -0300

    beginning of day 11

---
+++ day-11_blackjack/challenge_11_blackjack.py
@@ -0,0 +1,64 @@
+#Final Project Day 11 - Blackjack Game
+#21st November 2024
+#Her link: https://appbrewery.github.io/python-day11-demo
+
+#My solution only watching the first explanation
+import random
+
+run = True
+while run:
+    user_cards = []
+    computer_cards = []
+    play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+    if play in ("Y","y"):
+        for num in range(2):
+            user_cards.append(random.randint(1,10))
+        #print(user_cards)
+        computer_cards.append(random.randint(1,10))
+        #print(computer_cards)
+
+        print(f"Your cards: {user_cards}. Your current score: " + str(sum(user_cards)))
+        print(f"Computer's first card: {computer_cards}")
+
+        get_card = input("Type 'y' to get another card, or type 'n' to pass: ")
+        while get_card in ("Y","y"):
+            user_cards.append(random.randint(1,10))
+
+            if sum(user_cards) > 21:
+                print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+                print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+                print("You went over 21. You lose!")
+
+                get_card = "n"
+                #END
+            elif sum(user_cards) == 21:
+                print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+                print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+                print("You got 21. You win!")
+
+                get_card = "n"
+                #END
+            else:
+                print(f"Your cards: {user_cards}. Your current score: " + str(sum(user_cards)))
+                print(f"Computer's first card: {computer_cards}")
+                get_card = input("Type 'y' to get another card, or type 'n' to pass: ")
+        
+        #erro aqui, executa mesmo que o user perdeu
+        computer_cards.append(random.randint(1,10))
+
+        if sum(computer_cards) > sum(user_cards):
+            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+            print("You lose. Computer's hand is greater than yours.")
+        elif sum(computer_cards) == sum(user_cards):
+            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+            print("It's a draw. Equal hands.")
+        else:
+            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+            print("You win! Your hand is greater than computer.")
+        
+    else:
+        run = False
+        print("\nOk, you've decided not to play.\nBye!")
---
+++ day-11_blackjack/readme.md
@@ -0,0 +1,8 @@
+# Day 11 - 21st November 2024
+
+
+## Function
+
+
+### Final Project
+Blackjack Capstone Project
