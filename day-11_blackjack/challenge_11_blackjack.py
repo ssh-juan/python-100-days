9a838882ac7cc1f190c2bad79ad806032f9bb04b
JuanBSoares <juanbsoares@outlook.com>
2024-11-22 13:25:52 -0300

    error fixed

--- day-11_blackjack/challenge_11_blackjack.py
+++ day-11_blackjack/challenge_11_blackjack.py
@@ -30,34 +30,34 @@ while run:
                 print("You went over 21. You lose!")
 
                 get_card = "n"
-                #END
+                end = True
             elif sum(user_cards) == 21:
                 print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
                 print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
                 print("You got 21. You win!")
 
                 get_card = "n"
-                #END
+                end = True
             else:
                 print(f"Your cards: {user_cards}. Your current score: " + str(sum(user_cards)))
                 print(f"Computer's first card: {computer_cards}")
                 get_card = input("Type 'y' to get another card, or type 'n' to pass: ")
         
-        #erro aqui, executa mesmo que o user perdeu
-        computer_cards.append(random.randint(1,10))
+        if not end:
+            computer_cards.append(random.randint(1,10))
 
-        if sum(computer_cards) > sum(user_cards):
-            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
-            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
-            print("You lose. Computer's hand is greater than yours.")
-        elif sum(computer_cards) == sum(user_cards):
-            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
-            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
-            print("It's a draw. Equal hands.")
-        else:
-            print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
-            print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
-            print("You win! Your hand is greater than computer.")
+            if sum(computer_cards) > sum(user_cards):
+                print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+                print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+                print("You lose. Computer's hand is greater than yours.")
+            elif sum(computer_cards) == sum(user_cards):
+                print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+                print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+                print("It's a draw. Equal hands.")
+            else:
+                print(f"Your final hand is: {user_cards}. Your final score: " + str(sum(user_cards)))
+                print(f"Computer's final hand: {computer_cards}. Final score: " + str(sum(computer_cards)))
+                print("You win! Your hand is greater than computer.")
         
     else:
         run = False
