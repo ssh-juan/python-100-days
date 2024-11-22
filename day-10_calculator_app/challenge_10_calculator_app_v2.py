2bfc457b915708cb240160def50f800d407b1840
JuanBSoares <juanbsoares@outlook.com>
2024-11-21 22:17:33 -0300

    including v2 of calculator

--- day-10_calculator_app/challenge_10_calculator_app_v2.py
+++ day-10_calculator_app/challenge_10_calculator_app_v2.py
@@ -2,7 +2,8 @@
 #19nd November 2024
 #Her link: https://appbrewery.github.io/python-day10-demo
 
-#Solution using her way of solving
+#Solution following the steps of video, but it's still my solution
+import art
 
 def add(n1,n2):
     return n1 + n2
@@ -22,3 +23,31 @@ operations = {
 }
 
 #TODO: Use the dictionary operations to perform the calculations. Multiply 4 * 8 using the dictionary
+#print(operations["*"](8,4))
+
+print(art.logo)
+
+new_calc = True
+while new_calc:
+    n1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    operator = input("Pick an operation: ")
+    n2 = float(input("What's the second number? "))
+
+    result = operations[operator](n1,n2)
+
+    print(f"{n1} {operator} {n2} = {result}")
+
+    keep_result = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    while keep_result in ("Y","y"):
+        n1 = result
+        for symbol in operations:
+            print(symbol)
+        operator = input("Pick an operation: ")
+        n2 = float(input("What's the second number? "))
+        result = operations[operator](n1,n2)
+
+        print(f"{n1} {operator} {n2} = {result}")
+
+        keep_result = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
