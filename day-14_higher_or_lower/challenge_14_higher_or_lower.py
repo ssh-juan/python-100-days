db46afcb786202f56297bf2ebf42d9adb40e6b44
JuanBSoares <juanbsoares@outlook.com>
2024-11-28 16:26:32 -0300

    beginning of day 14

---
+++ day-14_higher_or_lower/challenge_14_higher_or_lower.py
@@ -0,0 +1,11 @@
+#Final Project Day 14 - Higher or Lower
+#28th November 2024
+#Her link: https://appbrewery.github.io/python-day14-demo
+import art
+
+score = 0
+
+print(art.logo)
+
+print(art.vs)
+#print(f"Sorry, that's wrong. Final score: {score})
---
+++ day-14_higher_or_lower/readme.md
@@ -0,0 +1,8 @@
+# Day 14 - 28th November 2024
+- Higher or Lower Game
+
+## Lessons Learned
+
+
+### Final Project
+Higher or Lower Game
